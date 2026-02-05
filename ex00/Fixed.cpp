43139cbb3172c8e090fe1189fe82797a92ff03f2
Gautier Ducurtil <gaducurt@student.42.fr>
2026-02-05 15:56:57 +0100

    first commit

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,49 @@
+#include "Fixed.hpp"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 16:52:37 by gaducurt          #+#    #+#             */
+/*   Updated: 2026/02/04 16:52:38 by gaducurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+Fixed::Fixed(): _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &obj)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = obj;
+}
+
+Fixed &Fixed::operator=(const Fixed &obj)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &obj)
+		this->_value = obj.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+    return (0);
+}
+
+void Fixed::setRawBit(int const raw)
+{
+	(void)raw;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
